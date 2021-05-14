4788db1646060207c12a8bbba41278c34548e01a
Sebastian Ayala Gonzalez <sebaxayala@gmail.com>
2021-05-13 19:44:30 -0500

    [ADD] Operaciones CRUD del API

--- backend/src/app.py
+++ backend/src/app.py
@@ -7,28 +7,64 @@ app.config['MONGO_URI'] = 'mongodb://localhost/pythonreactdb'
 
 mongo = PyMongo(app)
 
+CORS(app)
+
 collection_users = mongo.db.users
 
 
 @app.route('/users', methods=['POST'])
 def create_user():
-    return 'received'
+    id = collection_users.insert(
+        {
+            'name': request.json['name'],
+            'email': request.json['email'],
+            'password': request.json['password']
+        }
+    )
+    return jsonify({'id': str(ObjectId(id))})
 
 @app.route('/users', methods=['GET'])
 def get_users():
-    return 'received'
+    users = []
+    for doc in collection_users.find():
+        users.append(
+            {
+                '_id': str(ObjectId(doc['_id'])),
+                'name': doc['name'],
+                'email': doc['email'],
+                'password': doc['password'],
+            }
+        )
+    return jsonify(users)
 
 @app.route('/user/<id>', methods=['GET'])
-def get_user():
-    return 'received'
+def get_user(id):
+    user = collection_users.find_one({'_id': ObjectId(id)})
+    return jsonify({
+        '_id': str(ObjectId(user['_id'])),
+        'name': user['name'],
+        'email': user['email'],
+        'password': user['password'],
+    })
 
-@app.route('/users/<id>', methods=['GET'])
-def delete_user():
-    return 'received'
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    response = collection_users.delete_one({'_id': ObjectId(id)})
+    return jsonify({'msg': 'User deleted'})
 
 @app.route('/users/<id>', methods=['PUT'])
-def update_user():
-    return 'received'
+def update_user(id):
+    collection_users.update_one(
+        {'_id': ObjectId(id)},
+        {'$set':
+            {
+                'name': request.json['name'],
+                'email': request.json['email'],
+                'password': request.json['password']
+            }
+        }
+    )
+    return jsonify({'msg': 'User updated'})
 
 if __name__ == "__main__":
     app.run(debug=True)
